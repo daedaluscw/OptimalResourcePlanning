88ddafb5b076cb361ca13b6871dce2f1557b1a42
Christopher Wells <cwells21@masonlive.gmu.edu>
2021-03-04 21:50:23 -0500

    More Work on the resource class
    finished the add to array for the resource class

--- src/GenericResource.java
+++ src/GenericResource.java
@@ -29,9 +29,72 @@ public class GenericResource<T> {
 	 /*
 	  * Adds a resource element to the list is it does not already exist in the list.
 	  */
-	public Boolean addResource ( T quantity, String name)
+	public Boolean addResource ( T quantity, String name, Boolean consumable)
 	{
-		Boolean resultOfOperation = false; //The return value from the operation
+		Boolean resultOfOperation = false; //The return value from the operation used to determine the sucess of the operation
+		
+		if (checkTtype(quantity) == false )
+		{
+			return false;
+			//The value of the T is not an acceptable type
+		}
+		
+		if ( name.isEmpty() == true)
+		{
+			return false;
+			//The name string is empty and the operation can not be completed
+		}
+		
+		//The inital easy tests have passed and now a node has to be constructed to be added to the list
+		
+		resourceNode tempNode = new resourceNode();
+		
+		resultOfOperation = setNameOfResource( tempNode, name);	//Calls the function to set the name of the node
+		if (resultOfOperation == false)
+		{
+			return false;	//Check the return to ensure that the name of the temporary node was set.
+		}
+		
+		resultOfOperation = setQuantityOfResource( tempNode, quantity);
+		if (resultOfOperation == false)
+		{
+			return false;	//Check the return to ensure that the quantity of the temporary node was set.
+		}
+		
+		resultOfOperation = setConsumable( tempNode, consumable);
+		if (resultOfOperation == false)
+		{
+			return false;	//Check the return to ensure that the consumable value of the temporary node was set.
+		}
+		
+		return true;
+	}
+	
+	private boolean checkTtype ( T type)
+	{
+		Boolean returnValue = false; //Check to see if the type is one of the types that are supported as a value.
+		
+		if ( type instanceof Integer)
+		{
+			return true;
+		}
+		
+		if (type instanceof Float)
+		{
+			return true;
+		}
+		
+		if (type instanceof Long)
+		{
+			return true;
+		}
+		
+		return returnValue;
+	}
+		
+		
+		
+		
 		
 	}
 	public static void main(String[] args) {
